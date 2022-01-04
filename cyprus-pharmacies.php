5fa28c06d2fa3062e18a1a5667c6cc16abf6b35d
Savvas Hadjigeorgiou <savvasha@hotmail.com>
2022-01-04 16:13:31 +0200

    First commit

---
+++ cyprus-pharmacies.php
@@ -0,0 +1,58 @@
+<?php
+/**
+Plugin Name: Cyprus Pharmacies
+Description: An easy way to show the all-night pharmacies of Cyprus per city.
+Author: Savvas
+Author URI: https://profiles.wordpress.org/savvasha/
+Version: 1.0.0
+Requires at least: 5.3
+Requires PHP: 7.2
+License: GPL v2 or later
+License URI: https://www.gnu.org/licenses/gpl.html
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+if ( ! class_exists( 'Cyprus_Pharmacies' ) ) :
+
+/**
+ * Main Cyprus Pharmacies Class
+ *
+ * @class Cyprus_Pharmacies
+ * @version	1.1.0
+ */
+class Cyprus_Pharmacies {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		// Define constants
+		$this->define_constants();
+
+		// Include required files
+		$this->includes();
+		
+	}
+	
+	/**
+	 * Define constants
+	*/
+	private function define_constants() {
+		if ( !defined( 'cypharm_PLUGIN_BASE' ) )
+			define( 'cypharm_PLUGIN_BASE', plugin_basename( __FILE__ ) );
+	}
+
+	/**
+	 * Include required files
+	*/
+	private function includes() {
+			//load the needed frontend files
+			include( dirname( __FILE__ ) . '/includes/class-cypharm-front.php' );
+	}
+}
+
+endif;
+
+new Cyprus_Pharmacies();
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Cyprus Pharmacies ===
+Contributors: savvasha
+License: GPLv2 or later
+Tags: Cyprus, pharmacy, pharmacies, greek
+Requires at least: 5.3
+Tested up to: 5.8.1
+Requires PHP: 5.6.40
+Stable tag: 1.0.0
+
+Show all-night pharmacies of Cyprus
+
+== Description ==
+An easy way to show the all-night today and tomorrow pharmacies of Cyprus per city (Nicosia, Limassol, Larnaca, Paralimni, Paphos).
+
+A simple example
+
+`[cypharm show_title=false]`
+
+**More Options**
+
+The plugin supports the following optional parameters:
+
+* `city`: select the Cyprus city like Limassol, Paphos, Nicosia etc. (defaults to `Paphos`)
+* `title`: Choose a custom title to use (defaults to `false`)
+
+= Credits =
+
+* [Εθνική Διαδικτυακή Πύλη Ανοικτών Δεδομένων](https://www.data.gov.cy/)
+
+== Screenshots ==
+
+1.  The all-night pharmacies of Paphos for 4th and 5th of January
+
+== Changelog ==
+
+= 1.0.0 =
+
+* First Release!
