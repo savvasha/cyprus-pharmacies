e2e156ad50a619b8fe8cc2e20e857afbd71cf071
savvasha <savvasha@hotmail.com>
2023-03-16 18:23:44 +0200

    Add extra checks before output any data

--- includes/class-cypharm-front.php
+++ includes/class-cypharm-front.php
@@ -66,7 +66,7 @@ class CyPharm_Front {
 		$output .= '<h3>' . date_i18n( 'l' ) . ' , ' . date_i18n( 'j' ) . ' ' . date_i18n( 'M' ) . ' ' . date_i18n( 'Y' ) . '<h3>';
 
 		foreach ( $today_pharmacies as $today_pharmacy ) {
-			if ( isset( $coordinates[ $atts['city'] ][ $today_pharmacy->reg_no ] ) ) {
+			if ( isset( $today_pharmacy->reg_no ) && isset( $coordinates[ $atts['city'] ][ $today_pharmacy->reg_no ] ) && !isset( $today_pharmacy->geolocation ) ) {
 				$lat              = $coordinates[ $atts['city'] ][ $today_pharmacy->reg_no ]['Latitude'];
 				$long             = $coordinates[ $atts['city'] ][ $today_pharmacy->reg_no ]['Longitude'];
 				$pharmacy_address = '<a href="http://www.google.com/maps/place/' . $lat . ',' . $long . '" target="_blank">' . $today_pharmacy->address . '</a>';
@@ -76,10 +76,18 @@ class CyPharm_Front {
 			$output .= '<h4>' . $today_pharmacy->surname . ' ' . $today_pharmacy->name . '</h4>';
 			$output .= '<ul>';
 			$output .= '<li><strong>' . esc_html__( 'Address:', 'cyprus-pharmacies' ) . '</strong> ' . $pharmacy_address . '</li>';
-			$output .= '<li><strong>' . esc_html__( 'Additional Address info:', 'cyprus-pharmacies' ) . '</strong> ' . $today_pharmacy->additional_address . '</li>';
-			$output .= '<li><strong>' . esc_html__( 'Municipality/Community:', 'cyprus-pharmacies' ) . '</strong> ' . $today_pharmacy->municipalitycommunity . '</li>';
-			$output .= '<li><strong>' . esc_html__( 'Pharmacy Telephone:', 'cyprus-pharmacies' ) . '</strong> <a href="tel:' . $today_pharmacy->pharmacy_tel_no . '"> ' . $today_pharmacy->pharmacy_tel_no . ' </a></li>';
-			$output .= '<li><strong>' . esc_html__( 'Home Telephone:', 'cyprus-pharmacies' ) . '</strong> <a href="tel:' . $today_pharmacy->house_tel_no . '"> ' . $today_pharmacy->house_tel_no . ' </a></li>';
+			if ( isset ( $today_pharmacy->additional_address ) ) {
+				$output .= '<li><strong>' . esc_html__( 'Additional Address info:', 'cyprus-pharmacies' ) . '</strong> ' . $today_pharmacy->additional_address . '</li>';
+			}
+			if ( isset ( $today_pharmacy->municipalitycommunity ) ) {
+				$output .= '<li><strong>' . esc_html__( 'Municipality/Community:', 'cyprus-pharmacies' ) . '</strong> ' . $today_pharmacy->municipalitycommunity . '</li>';
+			}
+			if ( isset ( $today_pharmacy->pharmacy_tel_no ) ) {
+				$output .= '<li><strong>' . esc_html__( 'Pharmacy Telephone:', 'cyprus-pharmacies' ) . '</strong> <a href="tel:' . $today_pharmacy->pharmacy_tel_no . '"> ' . $today_pharmacy->pharmacy_tel_no . ' </a></li>';
+			}
+			if ( isset ( $today_pharmacy->house_tel_no ) ) {
+				$output .= '<li><strong>' . esc_html__( 'Home Telephone:', 'cyprus-pharmacies' ) . '</strong> <a href="tel:' . $today_pharmacy->house_tel_no . '"> ' . $today_pharmacy->house_tel_no . ' </a></li>';
+			}
 			$output .= '</ul>';
 		}
 
@@ -89,7 +97,7 @@ class CyPharm_Front {
 		$output .= '<h3>' . date_i18n( 'l', strtotime( '+1 day' ) ) . ' , ' . date_i18n( 'j', strtotime( '+1 day' ) ) . ' ' . date_i18n( 'M', strtotime( '+1 day' ) ) . ' ' . date_i18n( 'Y', strtotime( '+1 day' ) ) . '<h3>';
 
 		foreach ( $tomorrow_pharmacies as $tomorrow_pharmacy ) {
-			if ( isset( $coordinates[ $atts['city'] ][ $tomorrow_pharmacy->reg_no ] ) ) {
+			if ( isset( $tomorrow_pharmacy->reg_no ) &&  isset( $coordinates[ $atts['city'] ][ $tomorrow_pharmacy->reg_no ] ) ) {
 				$lat              = $coordinates[ $atts['city'] ][ $tomorrow_pharmacy->reg_no ]['Latitude'];
 				$long             = $coordinates[ $atts['city'] ][ $tomorrow_pharmacy->reg_no ]['Longitude'];
 				$pharmacy_address = '<a href="http://www.google.com/maps/place/' . $lat . ',' . $long . '" target="_blank">' . $tomorrow_pharmacy->address . '</a>';
@@ -99,10 +107,18 @@ class CyPharm_Front {
 			$output .= '<h4>' . $tomorrow_pharmacy->surname . ' ' . $tomorrow_pharmacy->name . '</h4>';
 			$output .= '<ul>';
 			$output .= '<li><strong>' . esc_html__( 'Address:', 'cyprus-pharmacies' ) . '</strong> ' . $pharmacy_address . '</li>';
-			$output .= '<li><strong>' . esc_html__( 'Additional Address info:', 'cyprus-pharmacies' ) . '</strong> ' . $tomorrow_pharmacy->additional_address . '</li>';
-			$output .= '<li><strong>' . esc_html__( 'Municipality/Community:', 'cyprus-pharmacies' ) . '</strong> ' . $tomorrow_pharmacy->municipalitycommunity . '</li>';
-			$output .= '<li><strong>' . esc_html__( 'Pharmacy Telephone:', 'cyprus-pharmacies' ) . '</strong> <a href="tel:' . $tomorrow_pharmacy->pharmacy_tel_no . '"> ' . $tomorrow_pharmacy->pharmacy_tel_no . ' </a></li>';
-			$output .= '<li><strong>' . esc_html__( 'Home Telephone:', 'cyprus-pharmacies' ) . '</strong> <a href="tel:' . $tomorrow_pharmacy->house_tel_no . '"> ' . $tomorrow_pharmacy->house_tel_no . ' </a></li>';
+			if ( isset ( $tomorrow_pharmacy->additional_address ) ) {
+				$output .= '<li><strong>' . esc_html__( 'Additional Address info:', 'cyprus-pharmacies' ) . '</strong> ' . $tomorrow_pharmacy->additional_address . '</li>';
+			}
+			if ( isset ( $tomorrow_pharmacy->municipalitycommunity ) ) {
+				$output .= '<li><strong>' . esc_html__( 'Municipality/Community:', 'cyprus-pharmacies' ) . '</strong> ' . $tomorrow_pharmacy->municipalitycommunity . '</li>';
+			}
+			if ( isset ( $tomorrow_pharmacy->pharmacy_tel_no ) ) {
+				$output .= '<li><strong>' . esc_html__( 'Pharmacy Telephone:', 'cyprus-pharmacies' ) . '</strong> <a href="tel:' . $tomorrow_pharmacy->pharmacy_tel_no . '"> ' . $tomorrow_pharmacy->pharmacy_tel_no . ' </a></li>';
+			}
+			if ( isset ( $tomorrow_pharmacy->house_tel_no ) ) {
+				$output .= '<li><strong>' . esc_html__( 'Home Telephone:', 'cyprus-pharmacies' ) . '</strong> <a href="tel:' . $tomorrow_pharmacy->house_tel_no . '"> ' . $tomorrow_pharmacy->house_tel_no . ' </a></li>';
+			}
 			$output .= '</ul>';
 		}
 
